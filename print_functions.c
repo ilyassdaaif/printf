30f5cae2d606a24fa090277005623266088b5bf4
ilyass <ilyass.df@gmail.com>
2023-08-02 08:22:48 -0700

    a

--- print_functions.c
+++ print_functions.c
@@ -55,8 +55,8 @@ int print_string(va_list ap, params_t *params)
 
 	(void)params;
 	switch ((int)(!str))
-	case 1:
-		str = NULL_STRING;
+		case 1:
+			str = NULL_STRING;
 
 	j = pad = _strlen(str);
 	if (params->precision < pad)
